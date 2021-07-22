6443ef85e9e1e994d7f730e60dfbe7beb3a7ead1
glbreeze <gl.breeze@gmail.com>
2021-07-22 03:03:53 -0400

    add_experiment_rfunet_ca6_02

---
+++ experiments/rfunet_ca_02/train_ca6_005.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=ca6_005
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=lg154@nyu.edu
+#SBATCH --output=ca6_005.out
+#SBATCH --gres=gpu:1 # How much gpu need, n is the number
+
+module purge
+source ~/.bashrc
+source /scratch/lg154/anaconda3/bin/activate
+source activate python36
+module load cuda/10.2.89 
+#module load cudnn/7.5
+
+mkdir -p log 
+python train.py --mmf_att 'CA6' --lr '0.005' >log/train_ca6_005.log 2>& 1
+echo "FINISH"
