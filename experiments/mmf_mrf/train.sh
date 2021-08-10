af0a1f7f4501ddd2956b4c226e1ff734cb3cd4ac
Li Guo <gl.breeze@gmail.com>
2021-08-10 23:55:51 +0800

    Update train.sh

--- experiments/mmf_mrf/train.sh
+++ experiments/mmf_mrf/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=rfu
+#SBATCH --job-name=$1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
@@ -8,7 +8,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=lg154@nyu.edu
-#SBATCH --output=rfu.out
+#SBATCH --output=$1.out
 #SBATCH --gres=gpu # How much gpu need, n is the number
 
 module purge
